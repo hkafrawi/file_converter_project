0e37277499ca814a8e69d6e7438f6f9bdac7524f
Kafrawi <hassaballaelkafrawi@gmail.com>
2025-01-29 21:42:17 +0100

    Add: Adding read_csv function

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ def get_column_names(schemas,ds_name,sorting_key="column_position"):
     columns = sorted(column_details, key= lambda col:col[sorting_key] )
 
     return [col["column_name"] for col in columns]
+
+def read_csv(file, schemas):
+    file_details = re.split('[/\\\]', file)
+    ds_name = file_details[-2]
+    file_name = file_details[-1]
+    columns = get_column_names(schemas, ds_name)
+    df = pd.read_csv(file,names=columns)
+
+    return df
